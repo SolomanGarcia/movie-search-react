fc72ae6df3637faffbe41de90b8b73e563636fcc
Soloman Garcia <soloman.garcia@gmail.com>
2021-01-25 11:40:12 -0700

    create SearchMovies component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+import SearchMovies from "./components/SearchMovies";
+
 class Main extends React.Component {
   render() {
     return (
       <div className="container">
         <h1 className="title">React Movie Search</h1>
+        <SearchMovies />
       </div>
     );
   }
