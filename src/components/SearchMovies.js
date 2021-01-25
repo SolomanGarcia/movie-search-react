66b2c08129dd9b8aa91890074a5c2e3afb5c07fa
Soloman Garcia <soloman.garcia@gmail.com>
2021-01-25 12:09:56 -0700

    add .env

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -4,6 +4,8 @@ export default function SearchMovies() {
   const searchMovies = async (e) => {
     e.preventDefault();
     console.log("submitting");
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
   };
 
   return (
