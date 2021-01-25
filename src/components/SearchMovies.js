515393289c885537b80d6b6eb22c2ad063733682
Soloman Garcia <soloman.garcia@gmail.com>
2021-01-25 12:02:56 -0700

    clear errors in console

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 export default function SearchMovies() {
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    console.log("submitting");
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={searchMovies}>
       <label htmlFor="query" className="label">
         Movie Name
       </label>
